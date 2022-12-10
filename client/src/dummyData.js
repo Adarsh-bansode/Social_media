8ea0fed0e5bbf63a73eeaa47cad75021d95737db
adarsh <adarshbansode@gmail.com>
2022-12-10 15:48:26 +0530

    Initial commit

---
+++ client/src/dummyData.js
@@ -0,0 +1,130 @@
+export const Users = [
+  {
+    id:1,
+    profilePicture: "assets/profiles/kishor.jpg",
+    username: "Kishor Tekale",
+  },
+  {
+    id:2,
+    profilePicture: "assets/profiles/krishna.jpg",
+    username: "Soham Marewar",
+  },
+  {
+    id:3,
+    profilePicture: "assets/profiles/omkar.jpg",
+    username: "Omkar Jadhav",
+  },
+  {
+    id:4,
+    profilePicture: "assets/profiles/vaibhav.jpg",
+    username: "Vaibhav Khadke",
+  },
+  {
+    id:5,
+    profilePicture: "assets/profiles/adarsh.jpg",
+    username: "Adarsh Bansode",
+  },
+
+
+  {
+    id:6,
+    profilePicture: "assets/profiles/mahesh.jpg",
+    username: "Mahesh Uike",
+  },
+ 
+];
+
+export const Posts = [
+  {
+    id: 1,
+    desc: "Love For All, Hatred For None.",
+    photo: "assets/profiles/kishor1.jpg",
+    date: "5 mins ago",
+    userId: 1,
+    like: 1564,
+    comment: 9,
+  },
+  {
+    id: 2,
+    photo: "assets/profiles/krishna.jpg",
+    date: "15 mins ago",
+    userId: 2,
+    like: 222,
+    comment: 1,
+  },
+  {
+    id: 3,
+    desc: "Every moment is a fresh beginning.",
+    photo: "assets/profiles/omkar.jpg",
+    date: "1 hour ago",
+    userId: 3,
+    like: 61,
+    comment: 2,
+  },
+  {
+    id: 4,
+    photo: "assets/profiles/vaibhav.jpg",
+    date: "4 hours ago",
+    userId: 4,
+    like: 7,
+    comment: 3,
+  },
+  {
+    id: 5,
+    photo: "assets/profiles/adarsh.jpg",
+    date: "5 hours ago",
+    userId: 5,
+    like: 23,
+    comment: 5,
+  },
+  {
+    id: 6,
+    photo: "assets/profiles/group.jpg",
+    date: "1 day ago",
+    userId: 6,
+    like: 44,
+    comment: 6,
+  },
+  {
+    id: 7,
+    desc: "Never regret anything that made you smile.",
+    photo: "assets/profiles/chhagan.jpg",
+    date: "2 days ago",
+    userId: 7,
+    like: 52,
+    comment: 3,
+  },
+  {
+    id: 8,
+    photo: "assets/profiles/pramod .jpg",
+    date: "3 days ago",
+    userId: 8,
+    like: 1115,
+    comment: 1,
+  },
+  {
+    id: 9,
+    desc: "Change the world by being yourself.",
+    photo: "assets/profiles/mahesh.jpg",
+    date: "5 days ago",
+    userId: 9,
+    like: 1111,
+    comment: 2,
+  },
+  {
+    id: 10,
+    photo: "assets/profiles/abhay.jpg",
+    date: "1 week ago",
+    userId: 10,
+    like: 104,
+    comment: 12,
+  },
+  {
+    id: 11,
+    photo: "assets/profiles/lokesh.jpg",
+    date: "2 week ago",
+    userId: 11,
+    like: 324,
+    comment: 62,
+  },
+];
